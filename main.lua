76c8545dfc56ea50c5a5caf7cc6bf4fea5475ec2
Sesso Kosga <kosgasesso@gmail.com>
2022-04-27 12:00:56 +0100

    Added rooms

--- main.lua
+++ main.lua
@@ -1,10 +1,78 @@
--- title:  Adv Game
+-- title:  Escpace Adventure
 -- author: Sesso Kosga
--- desc:   Adventure Game
+-- desc:   Adventure Game, escape from your burning house
 -- script: lua
+lstR={}
+lstInv={}
+curR=nil
+
+function addR()
+	local r={}
+	r.na=""
+	r.id=""
+	r.see={}
+	r.e=""
+	r.w=""
+	r.n=""
+	r.s=""
+	table.insert(lstR,r)
+	return r
+end
+
+
+r_p = addR()
+r_p.na="Principal room"
+r_p.id="PRINCIPAL"
+r_p.see={"Door"}
+r_p.n="COULOIR1"
+r_p.e="COULOIR2"
+
+r_c1=addR()
+r_c1.na="Couloir"
+r_c1.id="COULOIR1"
+r_c1.w="ROOM"
+r_c1.s="PRINCIPAL"
+
+r_c2=addR()
+r_c2.na="Couloir"
+r_c2.id="COULOIR2"
+r_c2.w="PRINCIPAL"
+r_c2.e="KITCHEN"
+
+r_r=addR()
+r_r.na="Room"
+r_r.id="ROOM"
+r_r.see={"BOX"}
+r_r.e="COULOIR1"
+
+r_k=addR()
+r_k.na="Kitchen"
+r_k.id="KITCHEN"
+r_k.w="COULOIR2"
+r_k.see={"HAMMER"}
+
+curR = r_p
 
 function TIC()
-
+	cls()
+	dir=""	
+	print(curR.na,20,0,12)
+	-- Directions
+	if curR.e~="" then
+		dir=dir.."E"
+	end
+	if curR.w~="" then
+		dir=dir.." W"
+	end
+		if curR.n~="" then
+		dir=dir.." N"
+	end	if curR.s~="" then
+		dir=dir.." S"
+	end
+	print(dir,5,40,12)
+	
+	
+	
 end
 
 -- <TILES>
